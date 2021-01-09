8da77708d35f7d4c15a6f91569da0329be5d62c6
Mobydack <devzetsubou@gmail.com>
2021-01-09 08:37:32 +0300

    create structure Plane

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+struct Plane {
+    data: [u8; 9],
 }
+
+impl Plane {
+    fn new() -> Plane {
+        Plane {
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        }
+    }
+}
+
+fn main() {
+    let plane = Plane::new();
+
+    println!("{:#?}", plane.data);
+}
+/*
+
+[   0  1  2
+--------------
+    0, 0, 0, | 0
+    0, 0, 0, | 1
+    0, 0, 0, | 2
+]
+*/
